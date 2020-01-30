e05a02344665a82ebbf3ffbe485e1af45fa6dcb2
Ben Dodson <bdodson@snapchat.com>
2020-01-29 22:37:36 -0800

    update init

--- init.sh
+++ init.sh
@@ -21,32 +21,8 @@ main() {
     fi
 
     echo "Looking for component graph files in $build_dir"
-
-    local components_manifest_file="build/ComponentsManifest.json"
-    mkdir -p build
-    local tmp="${components_manifest_file}.tmp"
-    find $build_dir -path '*/build/*_graph.json' >"$tmp"
-    echo '{"components": [' >|"$components_manifest_file"
-    for f in $(awk "NR != $(wc -l <$tmp)" $tmp); do
-        cat "$f" >>"$components_manifest_file"
-        echo ',' >>"$components_manifest_file"
-    done
-    f=$(tail -n 1 "$tmp")
-
-    if [ -z "$f" ]; then
-        echo "Failed to find any component graph files in $build_dir. Did you add the SPI plugin to your modules?"
-        exit 1
-    fi
-
-    cat "$f" >>"$components_manifest_file"
-    echo ']}' >>"$components_manifest_file"
-    
-    # clean up the tmp file
-    rm -f -- "$tmp"
-
-    # Copy the manifest 
-    cp -fR $components_manifest_file browser/public/$MANIFEST_FILENAME
-
+    `./scripts/mkmanifest.sh browser/public/$MANIFEST_FILENAME`
+ 
     # TODO: Generate the class info file
     local class_info_file="build/ClassInfo.json"
     echo '{}' >|"$class_info_file"

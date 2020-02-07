ed7296fdfabf37dbd3fa2c57bca9ba9b1896b515
Build-agent <build-agent@snapchat.com>
2020-02-07 04:48:07 +0000

    Updates

--- plaid/service-worker.js
+++ plaid/service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://daggerbrowser.com/plaid/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

16b592fe05575a92af2a4991cabf5b6ddffcb3e7
Raul Perez <rperez4@snapchat.com>
2021-09-28 15:08:54 -0700

    added test cases for parseString()

--- browser/src/__tests__/NodeAutosuggest.test.tsx
+++ browser/src/__tests__/NodeAutosuggest.test.tsx
@@ -1,22 +1,49 @@
-import {shallow, configure} from 'enzyme';
+import { configure} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16'
 import {expect} from 'chai';
 import NodeAutoSuggestService from "../service/NodeAutosuggestService"
-import React from 'react';
 
 
 
 configure({adapter: new Adapter()});
 
 describe("Map of class to Provider has correct short name", () => {
-    test("should display a blank login form, with remember me checked by default", async () => {
-      // ???
-    });
 
-    test('sum 1 + 2 = 3' , () => {
-      const nodeContainer = shallow(<NodeAutoSuggestService/>);
-      expect((nodeContainer.instance() as NodeAutoSuggestService).add(1,2)).equals(3)
+    test('test 1', () =>{
+      const nodeAutoSuggest = new NodeAutoSuggestService();
+      expect(nodeAutoSuggest.parseString(
+        "java.util.Map<java.lang.Class<? extends com.snap.durablejob.DurableJob<?>>,javax.inject.Provider<"+
+        "com.snap.durablejob.DurableJobProcessor<?,?>>>")).equals(
+        "Map<Class<DurableJob<?>> Provider<DurableJobProcessor<?>>>"
+        )
     })
 
+    test('test 2', () => {
+      const nodeAutoSuggest = new NodeAutoSuggestService();
+      expect(nodeAutoSuggest.parseString("com.snap.dagger.browser.sample.CoffeeApp.CoffeeShop")).equals(
+        "CoffeeApp.CoffeeShop")
+    })
+
+    test('test 3', () => {
+      const nodeAutoSuggest = new NodeAutoSuggestService();
+      expect(nodeAutoSuggest.parseString(
+        "java.util.Map<java.lang.Class<? extends com.snap.durablejob.DurableJob<?>>,javax.inject.Provider<"+
+        "com.snap.durablejob.DurableJobProcessor<?,?>>> com.snap.lenses.app.LensesDurableJobComponent_MultibindingsModule"+
+        "#provide_socialUnlockResponseCacheCleanupProcessorMultibinding")).equals(
+           "Map<Class<DurableJob<?>> Provider<DurableJobProcessor<?>>> LensesDurableJobComponentMultibindingsModule"
+          )
+    })
+
+    test('test 4', () => {
+      const nodeAutoSuggest = new NodeAutoSuggestService();
+      expect(nodeAutoSuggest.parseString("@io.plaidapp.core.dagger.DesignerNewsApi okhttp3.OkHttpClient")).equals(
+        "DesignerNewsApiOkHttpClient")
+    })
+
+    test('test 5', () => { 
+      const nodeAutoSuggest = new NodeAutoSuggestService();
+      expect(nodeAutoSuggest.parseString("io.plaidapp.core.dribbble.data.search.DribbbleSearchConverter.Factory")).equals(
+        "DribbbleSearchConverter.Factory")
+    })
   });
 
--- browser/src/service/NodeAutosuggestService.tsx
+++ browser/src/service/NodeAutosuggestService.tsx
@@ -1,15 +1,71 @@
 import React from "react"
 
-export class NodeAutosuggestService extends React.Component{
-    
-    add(a: number, b: number){
-        return a+b
+export class NodeAutosuggestService {
+    //constructor is empty, only necessary to access methods parseString, and addQuestionsMarks
+    constructor() {}
+
+    /* 
+    Component is passed in and a shorter name of the component is returned, 
+    Time Complexity O(n)
+    Space Complexity O(n)
+    */
+    parseString(component: string) {
+        var shorter_component = ""
+        for (var i = 0; i < component.length; i++) {
+            let c = component.charAt(i)
+            if (c == '#') {
+                break;
+            }
+            else if (c == ">") {
+                shorter_component += c
+            }
+            else if (c == "<") {
+                shorter_component += c;
+                i++
+                if (component.charAt(i) === component.charAt(i).toUpperCase() && component.charAt(i) != " ") {
+                    while (i < component.length && component.charAt(i) != ' ' && component.charAt(i).match(/[a-zA-Z.]/i)) {
+                        let build = component.charAt(i)
+                        shorter_component+= build
+                        i++
+                    }
+                }
+                //decrement to go back to previous position where the condition above wasn't met 
+                i--
+            }
+            else if (c === c.toUpperCase() && c != " " && c.match(/[a-zA-Z]/i)) {
+                shorter_component += c
+                i++
+                while (i < component.length && component.charAt(i) != ' ' && component.charAt(i).match(/[a-zA-Z.]/i)) {
+                    let build = component.charAt(i)
+                    shorter_component+= build
+                    i++
+                }
+                //decrement to go back to previous position where the condition above wasn't met 
+                i--
+            }
+        }
+        return this.addQuestionMarks(shorter_component)
     }
-       render(){
-           return(
-            <div>
-            </div>
-           )
-       }
+    /* 
+    After the component is parsed, question marks and spacing is added where conditions are met
+    Time Complexity O(n)
+    Space Complexity O(n)
+    */
+    addQuestionMarks(component: string) {
+        var shorter_component = ""
+        for (var i = 0; i < component.length; i++) {
+            let c = component.charAt(i)
+            shorter_component+= c
+            if (i < component.length-1 && c == '<' && component.charAt(i+1) == '>') {
+                shorter_component += "?"
+            }
+            else if (i < component.length-1 && c == '>' && component.charAt(i+1) != '>') {
+                shorter_component += " "
+            }
+        }
+        return shorter_component.toString()
+    }
 }
 export default NodeAutosuggestService
+
+// DesignerNewsApiOkHttpClient
